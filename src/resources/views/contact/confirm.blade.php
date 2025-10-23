d097dd2cabb36d285c886f43abd7959e5e2e6740
takayashiki <07rena01ichika@gmail.com>
2025-10-23 11:07:24 +0900

    確認画面の修正完了

--- src/resources/views/contact/confirm.blade.php
+++ src/resources/views/contact/confirm.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Confirm</title>
-    <!-- CSSパスはasset()ヘルパーを使用 -->
     <link rel="stylesheet" href="{{ asset('css/contact/confirm.css') }}">
 </head>
 <body>
@@ -17,81 +16,70 @@
         
         <form action="{{ route('contact.send') }}" method="POST" class="confirm-form">
             @csrf
+            
+            {{-- お問い合わせの種類の日本語名を取得するためのPHPブロックを事前に定義 --}}
+            @php
+                $categories = [
+                    1 => '商品のお届けについて',
+                    2 => '商品の交換について',
+                    3 => '商品トラブル',
+                    4 => 'ショップへのお問い合わせ',
+                    5 => 'その他',
+                ];
+            @endphp
+            
             <div class="confirm-table">
-                <!-- お名前 -->
                 <div class="confirm-row">
                     <div class="confirm-label">お名前</div>
                     <div class="confirm-value">{{ $contact['first_name'] }} {{ $contact['last_name'] }}</div>
-                    <!-- hidden: index.bladeのフィールド名と完全に一致させる -->
                     <input type="hidden" name="first_name" value="{{ $contact['first_name'] }}">
                     <input type="hidden" name="last_name" value="{{ $contact['last_name'] }}">
                 </div>
                 
-                <!-- 性別 (修正対象: short-row クラスを追加し、縦幅を狭める) -->
                 <div class="confirm-row short-row">
                     <div class="confirm-label">性別</div>
-                    <div class="confirm-value">
-                        {{ [
+                    <div class="confirm-value"><span>{{ 
+                        [
                             'male' => '男性', 
                             'female' => '女性', 
                             'other' => 'その他'
-                        ][$contact['gender']] ?? '不明' }}
-                    </div>
-                    <!-- hidden: index.bladeのフィールド名と完全に一致させる -->
+                        ][$contact['gender']] ?? '不明' 
+                    }}</span></div>
                     <input type="hidden" name="gender" value="{{ $contact['gender'] }}">
                 </div>
                 
-                <!-- メールアドレス -->
                 <div class="confirm-row">
                     <div class="confirm-label">メールアドレス</div>
                     <div class="confirm-value">{{ $contact['email'] }}</div>
                     <input type="hidden" name="email" value="{{ $contact['email'] }}">
                 </div>
                 
-                <!-- 電話番号 -->
                 <div class="confirm-row">
                     <div class="confirm-label">電話番号</div>
                     <div class="confirm-value">{{ $contact['tel1'] }}-{{ $contact['tel2'] }}-{{ $contact['tel3'] }}</div>
-                    <!-- hidden: index.bladeのフィールド名と完全に一致させる (tel1, tel2, tel3) -->
                     <input type="hidden" name="tel1" value="{{ $contact['tel1'] }}">
                     <input type="hidden" name="tel2" value="{{ $contact['tel2'] }}">
                     <input type="hidden" name="tel3" value="{{ $contact['tel3'] }}">
                 </div>
                 
-                <!-- 住所 -->
                 <div class="confirm-row">
                     <div class="confirm-label">住所</div>
                     <div class="confirm-value">{{ $contact['address'] }}</div>
                     <input type="hidden" name="address" value="{{ $contact['address'] }}">
                 </div>
                 
-                <!-- 建物名 -->
                 <div class="confirm-row">
                     <div class="confirm-label">建物名</div>
                     <div class="confirm-value">{{ $contact['building'] ?? 'なし' }}</div>
                     <input type="hidden" name="building" value="{{ $contact['building'] }}">
                 </div>
                 
-                <!-- お問い合わせの種類 (修正対象: short-row クラスを追加し、縦幅を狭める) -->
                 <div class="confirm-row short-row">
                     <div class="confirm-label">お問い合わせの種類</div>
-                    <div class="confirm-value">
-                        @php
-                            $categories = [
-                                1 => '商品のお届けについて',
-                                2 => '商品の交換について',
-                                3 => '商品トラブル',
-                                4 => 'ショップへのお問い合わせ',
-                                5 => 'その他',
-                            ];
-                        @endphp
-                        {{ $categories[$contact['category_id']] ?? '不明' }}
-                    </div>
-                    <!-- hidden: index.bladeのフィールド名と完全に一致させる -->
+                    <div class="confirm-value"><span>{{ $categories[$contact['category_id']] ?? '不明' }}</span></div>
                     <input type="hidden" name="category_id" value="{{ $contact['category_id'] }}">
                 </div>
                 
-                <!-- お問い合わせ内容 (修正対象: content-row クラスを維持し、縦幅を広げる) -->
                 <div class="confirm-row content-row">
                     <div class="confirm-label">お問い合わせ内容</div>
                     <div class="confirm-value">{{ $contact['content'] }}</div>
